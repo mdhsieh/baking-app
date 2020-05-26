7a35832ca291addfd1f9fc486247a2398b4ebf72
Michael Hsieh <mdhsieh8@gmail.com>
2020-05-25 17:00:45 -0700

    style: Comment out unecessary release of ExoPlayer.
    App crashes after several rotations with OutOfMemory ExoPlayer error
    on API 17 tablet.

--- app/src/main/java/com/michaelhsieh/bakingapp/RecipeStepDetailsFragment.java
+++ app/src/main/java/com/michaelhsieh/bakingapp/RecipeStepDetailsFragment.java
@@ -160,11 +160,11 @@ public class RecipeStepDetailsFragment extends Fragment {
 
         // release player and initialize again if ex. orientation changes
         // because don't want same audio playing twice
-        if (mExoplayer != null)
+        /*if (mExoplayer != null)
         {
             Log.d(TAG, "ExoPlayer is not null in onCreate, releasing player");
             mExoplayer.release();
-        }
+        }*/
 
         // Initialize the player.
         // get selected step's video URL
